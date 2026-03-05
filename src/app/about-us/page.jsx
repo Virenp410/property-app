68f039bbb9479508f533064b4754c6caef700bea
Kamlesh Chauhan <kamlesh.chauhan@seaneb.net>
2026-03-05 12:09:36 +0530

    Delete src directory

--- src/app/about-us/page.jsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-const getWebAppUrl = () =>
-  String(process.env.NEXT_PUBLIC_WEB_APP_URL || "").replace(/\/$/, "");
-
-export default function AboutUsRoutePage() {
-  redirect(`${getWebAppUrl()}/about-us`);
-}
